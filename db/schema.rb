7971221820a5a3598b109354c50652f017c61bbb
ggcodester <gracewqhe@gmail.com>
2026-04-22 04:44:07 -0500

    delete button tweaks and styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2026_04_22_022533) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
-
   create_table "activities", force: :cascade do |t|
     t.string "category"
     t.string "city"
